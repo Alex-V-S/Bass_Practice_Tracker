f398246408cfeddd81d0995f3d559fb693533049
Alejandro Villeda Schaeufler <aleuixcool@gmail.com>
2026-07-21 19:32:57 -0700

    fixed setup_db.py

--- setup_db.py
+++ setup_db.py
@@ -1,36 +1,32 @@
 import sqlite3
 
-# 1. Establish connection
-# This will automatically create 'bass_tracker.db' in current folder if it doesn't exist
+# 1. Establish the connection
 conn = sqlite3.connect('bass_tracker.db')
-
-# 2. Create a cursor object
-# The cursor is what is used to actually send SQL commands to the database/
 cursor = conn.cursor()
 
-# 3. Create the Techniques table
+# 2. Create the Techniques table (Static library)
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS Techniques(
+CREATE TABLE IF NOT EXISTS Techniques (
     technique_id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     category TEXT
-    )
+)
 ''')
 
-# 4. Create the Sessions table (Macro daily details)
+# 3. Create the Sessions table (Macro daily details)
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS Sessions (
     session_id INTEGER PRIMARY KEY AUTOINCREMENT,
     date TEXT NOT NULL,
-    duraction_minutes INTEGER
+    duration_minutes INTEGER
 )
 ''')
 
-# 5. Create the Session_Logs table (The bridge table with your metrics)
+# 4. Create the Session_Logs table (The bridge table with your metrics)
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS Session_Logs (
     log_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    session_id INTEGER.
+    session_id INTEGER,
     technique_id INTEGER,
     target_bpm INTEGER,
     achieved_bpm INTEGER,
@@ -44,13 +40,13 @@ CREATE TABLE IF NOT EXISTS Session_Logs (
 )
 ''')
 
-# 6. Insert a test technique to verify it works
+# 5. Insert a test technique to verify it works
 cursor.execute('''
 INSERT INTO Techniques (name, category)
 VALUES ('The Adults Are Talking - The Strokes', 'Repertoire')
 ''')
 
-# 7. Save (commit) the changes
+# 6. Save (commit) the changes and close
 conn.commit()
 conn.close()
 print("Database, tables, and test data created successfully!")
